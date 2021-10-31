5cce0d805d53034d8d278c2b51081525622ce0c0
NamoSmith <namosmith2545@gmail.com>
2021-10-31 11:17:22 +0700

    Add function for sign_in_and_sign_up.

--- forums/views.py
+++ forums/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def main(request):
     return render(request, 'forums/main.html')
+
+
+def sign_in_and_sign_up(request):
+    return render(request, 'forums/sign_in_and_sign_up.html')
