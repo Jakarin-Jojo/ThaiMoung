be0404586d5e5303b038eae3757730565b3c3f07
XOQDY <numfahmaek@gmail.com>
2021-11-07 15:47:40 +0700

    change path name urls for detail

--- forums/urls.py
+++ forums/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('detail/', views.detail, name='base'),
+    path('detail/', views.detail, name='detail'),
     path('create_forum/', views.CreateForumView.as_view(), name='create_forum'),
 ]
