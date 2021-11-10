12dd45e142fe526e826d66c56579d036be52b3a9
Jakarin Chujan <jojogamer67@gmail.com>
2021-11-11 00:25:55 +0700

    :lipstick: Update UI of Sign Up page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import PasswordInput
 
 
 class UserRegisterForm(UserCreationForm):
@@ -8,13 +9,15 @@ class UserRegisterForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         """Initialize a user form."""
         super(UserRegisterForm, self).__init__(*args, **kwargs)
-        self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget = PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Password'})
+        self.fields['password2'].widget = PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Password confirmation'})
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control'},),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
         }
