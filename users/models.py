a3d98dff23059047e8d03c654f52f52e1d605725
Jakarin Chujan <jojogamer67@gmail.com>
2021-11-18 02:14:29 +0700

    :card_file_box: Add bio texfiled to model user.

--- users/models.py
+++ users/models.py
@@ -5,7 +5,8 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png', upload_to='profile_pics')
+    image_profile = models.ImageField(default='profile_pics/default.png', upload_to='profile_pics')
+    bio = models.TextField(max_length=500, blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
