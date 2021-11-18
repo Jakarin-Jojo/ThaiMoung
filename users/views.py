021407be1cd315a588a7b7b5a5bf5f8be46af11c
Jakarin Chujan <jojogamer67@gmail.com>
2021-11-19 04:30:46 +0700

    :bug: Fix a bug of Updating username

--- users/views.py
+++ users/views.py
@@ -1,20 +1,15 @@
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 
 from forums.models import Post
 from .forms import UserUpdateForm, ProfileUpdateForm
 from .models import Profile
 
 
-@login_required(login_url='/accounts/login')
 def edit_profile(request, username):
-    user = User.objects.get(username=username)
-    user_profile = Profile.objects.get(user=user)
-    user_profile.save()
-
     if request.method == 'POST':
+
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
                                    request.FILES,
@@ -22,8 +17,7 @@ def edit_profile(request, username):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has been updated!')
-            return profile(request, username)
+            return redirect('main')
 
     else:
         u_form = UserUpdateForm(instance=request.user)
@@ -32,7 +26,6 @@ def edit_profile(request, username):
     context = {
         'u_form': u_form,
         'p_form': p_form,
-        'user_profile': user_profile
     }
 
     return render(request, 'users/edit_profile.html', context)
