271466d9cec50b4ff81c5e8ebed42816a6243ec4
Bhokin <bhokin.w@ku.th>
2021-11-29 01:44:55 +0700

    change status code

--- forums/tests/test_view.py
+++ forums/tests/test_view.py
@@ -14,7 +14,7 @@ class RegistrationViewTests(TestCase):
     def test_create_topic_view(self):
         """Tests that after we get into the page, we must get a status code of 200"""
         response = self.client.get(reverse('create_topic'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
     def test_create_forum_view(self):
         """Tests that after we get into the page, we must get a status code of 200"""
