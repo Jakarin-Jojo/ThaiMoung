10903b5e9b81380000618d1ea23182a296e371a0
Jakarin Chujan <jojogamer67@gmail.com>
2021-11-28 14:05:38 +0700

    :white_check_mark: Modify a test_E2E.py

--- forums/tests/test_E2E.py
+++ forums/tests/test_E2E.py
@@ -4,17 +4,14 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+import chromedriver_binary
 
 
 class E2E(LiveServerTestCase):
 
     def setUp(self) -> None:
         """Create an instance browser."""
-        self.my_options = Options()
-        self.my_options.headless = True
-        assert self.my_options.headless
-        self.browser = webdriver.Chrome(executable_path='C:/Users/Administratorz/Downloads/chromedriver.exe',
-                                        options=self.my_options)
+        self.browser = webdriver.Chrome()
 
     def test_access_to_ThaiMoung(self):
         """Test that a Chrome driver can get url of """
