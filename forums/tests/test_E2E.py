eecba3a93c0ff541e6315f89d799a766016fd346
Jakarin Chujan <jojogamer67@gmail.com>
2021-11-28 14:29:44 +0700

    :white_check_mark: Open the test E2E

--- forums/tests/test_E2E.py
+++ forums/tests/test_E2E.py
@@ -1,23 +1,23 @@
-# """Test E2E by using Selenium."""
-# from django.test import LiveServerTestCase
-#
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.chrome.options import Options
-# import chromedriver_binary
-#
-#
-# class E2E(LiveServerTestCase):
-#
-#     def setUp(self) -> None:
-#         """Create an instance browser."""
-#         self.browser = webdriver.Chrome()
-#
-#     def test_access_to_ThaiMoung(self):
-#         """Test that a Chrome driver can get url of """
-#         url = 'https://thaimoung.herokuapp.com/forums/'
-#         self.browser.get(url)
-#         self.assertEqual(url, self.browser.current_url)
-#
-#     def tearDown(self) -> None:
-#         self.browser.close()
+"""Test E2E by using Selenium."""
+from django.test import LiveServerTestCase
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import chromedriver_binary
+
+
+class E2E(LiveServerTestCase):
+
+    def setUp(self) -> None:
+        """Create an instance browser."""
+        self.browser = webdriver.Chrome()
+
+    def test_access_to_ThaiMoung(self):
+        """Test that a Chrome driver can get url of """
+        url = 'https://thaimoung.herokuapp.com/forums/'
+        self.browser.get(url)
+        self.assertEqual(url, self.browser.current_url)
+
+    def tearDown(self) -> None:
+        self.browser.close()
